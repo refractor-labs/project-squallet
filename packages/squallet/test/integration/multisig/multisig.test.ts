3834bef16378347d0aa617a2a2e6e47eee2eb393
Stewart <stewart@prysm.xyz>
2023-06-23 12:05:43 -0700

    fee too high test

--- packages/squallet/test/integration/multisig/multisig.test.ts
+++ packages/squallet/test/integration/multisig/multisig.test.ts
@@ -31,8 +31,8 @@ describe('MultiSig', () => {
 
     beforeAll(async () => {
       //setup
-      const createNew = true
-      const fund = true
+      const createNew = false
+      const fund = false
 
       const signerChronicle = new ethers.Wallet(
         process.env.LIT_PRIVATE_KEY as string,
@@ -48,10 +48,10 @@ describe('MultiSig', () => {
         pkpPromise = factoryCreatePkp({ signer: signerChronicle, ipfs: ipfsWrapper, code })
       } else {
         pkpPromise = Promise.resolve({
-          pkpId: '0x28736ccf83fbc0376fdfddfa96364232d8832a841a01455c1c452ebb6a78e953',
+          pkpId: '0x1291aae48f2e3098a368db977a338af469eebf978b6afb8c55188be6ec48bd8a',
           pkpPublicKey:
-            '0x0424bdd6817be95993c4100f5d51f6535e002a9a5abed52543d343eb5b9cd3709208521860c7e0e0bb0a9cf2345f7722214f0889039fa6e3cbeb889d0e3ea14f8e',
-          pkpAddress: '0xD2745FA34C730404c0BDfb7211912e10770C0DD9',
+            '0x04b87dab052561080876dd2b34fd09f14d1e623e9b3c5617da28aa385f7ddca04b75d1769ea0bbbfbc24a0d9e9b3c3c3663ec2c53e9e1ec7f40e59263558313ca0',
+          pkpAddress: '0xbba62e94168AF5A1Aba013fC29d88827a4Ab6A91',
           owner: '0x182351E16c1F511e50eA4438aFE3d0f16ae4769B',
           permittedActions: [],
           cid: 'QmZB93Z2ABJKPvzLaxfE55ioMt7PQnGzAphGE93g3n6q45'
@@ -162,11 +162,75 @@ describe('MultiSig', () => {
         maxFee: maxFee.toString()
       }
       const hash = hashTransactionRequest(tx)
+
+      for (let i = 1; i <= 7; i++) {}
+
+      let feeMult = [2, 7]
+      for (const mult of feeMult) {
+        const request: SignTransactionRequest = {
+          method: 'signTransaction',
+          request: {
+            fee: {
+              maxFeePerGas: maxFeePerGas.mul(mult).toString(),
+              maxPriorityFeePerGas: maxPriorityFeePerGas.toString()
+            },
+            signedTransaction: {
+              walletAddress: pkp.pkpAddress,
+              hash,
+              signatures: [
+                {
+                  signerAddress: ethers.utils.getAddress(signer.address),
+                  signature: await signer.signMessage(hash)
+                }
+              ],
+              transaction: tx
+            }
+          }
+        }
+        const res = await client.sendRequest(request)
+        console.log(mult, 'res', res)
+        if (!res.success) {
+          expect(res.success).toBeTruthy()
+        }
+      }
+    })
+
+    it('should prevent sign a transaction with correct signatures with fee over limit', async () => {
+      const fee = await provider.getFeeData()
+      const estimate = await provider.estimateGas({
+        to: pkp.pkpAddress,
+        from: pkp.pkpAddress,
+        value: '0x0'
+      })
+
+      const { maxFeePerGas, maxPriorityFeePerGas } = fee
+      if (!maxFeePerGas || !maxPriorityFeePerGas) {
+        throw new Error('no fee')
+      }
+
+      const maxFee = estimate.mul(maxFeePerGas).mul(7)
+
+      const tx: TransactionRequestI = {
+        to: pkp.pkpAddress,
+        from: signer.address,
+        nonce: 0,
+        gasLimit: estimate.toString(),
+        gasPrice: undefined,
+        data: '0x',
+        value: '0x0',
+        chainId: testnetChainId,
+        type: 2,
+        maxPriorityFeePerGas: undefined,
+        maxFeePerGas: undefined,
+        maxFee: maxFee.toString()
+      }
+      const hash = hashTransactionRequest(tx)
+
       const request: SignTransactionRequest = {
         method: 'signTransaction',
         request: {
           fee: {
-            maxFeePerGas: maxFeePerGas.mul(2).toString(),
+            maxFeePerGas: maxFeePerGas.mul(8).toString(),
             maxPriorityFeePerGas: maxPriorityFeePerGas.toString()
           },
           signedTransaction: {
@@ -184,9 +248,8 @@ describe('MultiSig', () => {
       }
       const res = await client.sendRequest(request)
       console.log('res', res)
-      if (!res.success) {
-        expect(res.success).toBeTruthy()
-      }
+      expect(res.success).toBe(false)
+      expect(res.data).toBe('fee too high')
     })
   })
 })
